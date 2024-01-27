790aed08a4c5c60c88822f0a5b67334f02445287
Hugo Vieira <108480477+Hugo2022vieira@users.noreply.github.com>
2024-01-27 12:37:13 -0300

    Update Exercicio 2.rb

--- Exercicio 2.rb
+++ Exercicio 2.rb
@@ -3,6 +3,7 @@ Exercício 2: Estruturas de Controle
 1.Crie uma variável chamada numero e atribua a ela um número inteiro.
 2.Crie uma estrutura condicional que imprima "O número é par." se o número for par, e "O número é ímpar." se for ímpar.
 =end
+
 numero = 10
 
 if numero % 2 == 0
