d8db71b30a0339ee7249dd9a94fe56919e53230f
Hugo Vieira <108480477+Hugo2022vieira@users.noreply.github.com>
2024-01-27 12:43:22 -0300

    Add files via upload

---
+++ Exercicio 3.rb
@@ -0,0 +1,13 @@
+=begin
+Exercício 3: Listas e Iteração
+1. Crie uma lista de 5 frutas.
+2.Use um loop para imprimir cada fruta da lista em uma linha.	
+=end
+
+# Cria uma lista de 5 frutas
+frutas = ["morango", "maçã", "banana", "laranja", "uva"]
+
+# Usa um loop para imprimir cada fruta da lista
+for fruta in frutas
+  puts fruta
+end
