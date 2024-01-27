5ab1328c0fda28d81361f3e6d88d3848047f70e9
Hugo Vieira <108480477+Hugo2022vieira@users.noreply.github.com>
2024-01-27 12:30:27 -0300

    Add files via upload

---
+++ Exercicio 1.rb
@@ -0,0 +1,11 @@
+=begin
+Exercício 1: Variáveis e Saída de Dados
+1.	Crie uma variável chamada nome e atribua a ela o seu nome.
+2.	Crie uma variável chamada idade e atribua a ela a sua idade.
+3.	Imprima uma mensagem na tela usando as variáveis criadas, algo como: "Olá, [nome]! Você tem [idade] anos."
+=end
+
+nome = "Programador"
+idade = 30
+
+puts "Olá, #{nome}! Você tem #{idade} anos."
