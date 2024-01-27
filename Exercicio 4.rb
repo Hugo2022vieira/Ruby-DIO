1a681a6b1107057c0cb34957ed5f09421a89416d
Hugo Vieira <108480477+Hugo2022vieira@users.noreply.github.com>
2024-01-27 12:48:28 -0300

    Add files via upload

---
+++ Exercicio 4.rb
@@ -0,0 +1,14 @@
+=begin
+Exercício 4: Funções
+1. Crie uma função chamada soma que aceite dois argumentos e retorne a soma deles.
+2. Chame a função com dois números diferentes e imprima o resultado.
+=end
+
+# Função para somar dois números
+def soma(a, b)
+  return a + b
+end
+
+# Chamada da função com dois números diferentes
+puts soma(1, 2) # => 3
+puts soma(5, 7) # => 12
