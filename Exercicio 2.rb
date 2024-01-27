2b972ab9323e1b3a4176c253f796f81735cdc4bc
Hugo Vieira <108480477+Hugo2022vieira@users.noreply.github.com>
2024-01-27 12:36:32 -0300

    Add files via upload

---
+++ Exercicio 2.rb
@@ -0,0 +1,12 @@
+=begin
+Exercício 2: Estruturas de Controle
+1.Crie uma variável chamada numero e atribua a ela um número inteiro.
+2.Crie uma estrutura condicional que imprima "O número é par." se o número for par, e "O número é ímpar." se for ímpar.
+=end
+numero = 10
+
+if numero % 2 == 0
+  puts "O número é par."
+else
+  puts "O número é ímpar."
+end
